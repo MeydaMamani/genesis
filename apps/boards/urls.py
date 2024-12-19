8c7abff2a172f8f51a021ff90d9170964ad72b03
MeydaMamani <meydamamani@gmail.com>
2024-12-19 12:53:16 -0500

    tablero de coberturas

--- apps/boards/urls.py
+++ apps/boards/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('operacional/', login_required(views.OperacionalView.as_view()), name='operacional'),
     path('desa/', login_required(views.DesaView.as_view()), name='desa'),
     path('prog/', login_required(views.ProgramasView.as_view()), name='programs'),
+    # path('fed/', login_required(views.FedView.as_view()), name='fed'),
+    # path('fed/child/', login_required(views.FedChild.as_view()), name='fed'),
+    # path('fed/filterDist/', login_required(views.DistrictView.as_view()), name='filter_dist'),
+    path('cobertura/', login_required(views.CoberturasView.as_view()), name='cobertura'),
 ]
