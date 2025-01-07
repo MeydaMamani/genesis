5b2b6cec88c54bee3fff49f1f0dbf405295857df
MeydaMamani <meydamamani@gmail.com>
2025-01-07 12:26:14 -0500

    corrigiendo radio button r40

--- apps/followup/static/js/js/plano.js
+++ apps/followup/static/js/js/plano.js
@@ -1,12 +1,10 @@
-$( document ).ready(function() {
-    $('input[name="myradio"]').change(function(e) {
-        if($(this).val() == 'r_r40'){
-            $(".r40").show();
-            $(".r40_doc").hide();
-        }
-        if($(this).val() == 'r_doc'){
-            $(".r40_doc").show();
-            $(".r40").hide();
-        }
-    });
+$('input[name="myradio"]').change(function(e) {
+    if($(this).val() == 'r_r40'){
+        $(".r40").show();
+        $(".r40_doc").hide();
+    }
+    if($(this).val() == 'r_doc'){
+        $(".r40_doc").show();
+        $(".r40").hide();
+    }
 });
